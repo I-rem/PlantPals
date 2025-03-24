5290f4379d3a0719dd1ee785a4da50c55d2a3fbd
I-rem <ayseirem.kayacioglu@gmail.com>
2025-03-24 13:27:54 +0300

    Add sprites

--- Assets/Scripts/Plant.cs
+++ Assets/Scripts/Plant.cs
@@ -3,29 +3,56 @@ using System.Collections;
 
 public class Plant : MonoBehaviour
 {
-    public Sprite[] growthStages; // Assign plant sprites in Inspector
+    [System.Serializable]
+    public class PlantType
+    {
+        public string name;
+        public Sprite[] growthStages;
+        public Sprite evolvedSprite;
+    }
+
+    public PlantType[] plantTypes;  // Assign different plant types in Inspector
+    private PlantType selectedPlant;
+    
     private SpriteRenderer spriteRenderer;
     private int growthStage = 0;
     private bool isWatered = false;
+    private bool isEvolved = false;
 
     void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
+
+        // Randomly select a plant type (or set it based on seed)
+        selectedPlant = plantTypes[Random.Range(0, plantTypes.Length)];
+
+        // Set initial sprite
+        spriteRenderer.sprite = selectedPlant.growthStages[0];
+
         StartCoroutine(Grow());
     }
 
     IEnumerator Grow()
     {
-        while (growthStage < growthStages.Length - 1)
+        while (growthStage < selectedPlant.growthStages.Length - 1)
         {
-            yield return new WaitForSeconds(isWatered ? 3f : 6f); // Watered plants grow faster
+            yield return new WaitForSeconds(isWatered ? 3f : 6f);
             growthStage++;
-            spriteRenderer.sprite = growthStages[growthStage];
+            spriteRenderer.sprite = selectedPlant.growthStages[growthStage];
         }
     }
 
     public void GrowFaster()
     {
         isWatered = true;
+    }
+
+    public void Evolve()
+    {
+        if (!isEvolved)
+        {
+            isEvolved = true;
+            spriteRenderer.sprite = selectedPlant.evolvedSprite;
+        }
     }
 }
