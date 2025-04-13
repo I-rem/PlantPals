c46a9de9737affaebe7c25ec55c9eee6511f982b
I-rem <ayseirem.kayacioglu@gmail.com>
2025-04-13 21:20:20 +0300

    Add textbox

--- Assets/Scripts/InteractionCollision.cs
+++ Assets/Scripts/InteractionCollision.cs
@@ -5,13 +5,36 @@ using UnityEngine;
 public class InteractionCollision : MonoBehaviour
 {
     public GameObject myText;
+    public GameObject textbox;
+    bool canInterract = false;
+
+    void Update()
+    {
+        if (canInterract && Input.GetKeyDown(KeyCode.E))
+        {
+            if (!textbox.activeInHierarchy)
+            {
+                canInterract = false;
+                myText.SetActive(false);
+                textbox.SetActive(true);
+            }
+            else
+            {
+                myText.SetActive(true);
+                textbox.SetActive(false);
+            }
+            
+        }
+    }
     void OnTriggerEnter2D(Collider2D other)
     {
         myText.SetActive(true);
+        canInterract = true;
     }
    
    void OnTriggerExit2D(Collider2D other)
    {
+        canInterract = false;
         myText.SetActive(false);
    }
 }
